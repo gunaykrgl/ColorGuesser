fdb703569d23e7870c0e138d3e8b89b40eb85a8a
Gunay <gunaykaraoglu21@gmail.com>
2023-07-20 22:45:07 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="box"></div>
+      <div className='button-container'>
+        <div className='button'>Button 1</div>
+        <div className='button'>Button 2</div>
+        <div className='button'>Button 3</div>
+        <div className='button'>Button 4</div>
+      </div>
+    </>
+  )
+}
+
+export default App
