5db53bd5b7f7c0338a10fa565798ea452113a997
Gunay <gunaykaraoglu21@gmail.com>
2023-07-22 14:09:55 +0300

    Add basic input handling for textbox

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Card.css"
 
 export default function Card(){
+    const [input, setInput] = useState("")
+    
+    function handleChange(e) {
+        setInput(e.target.value)        
+    }
+    
     return (
         <div id="container">
             <div className="box"></div>
-            <input type="text"></input>
+            <input type="text" 
+                value={input}
+                onChange={handleChange}
+                ></input>
         </div>
     )
 }
