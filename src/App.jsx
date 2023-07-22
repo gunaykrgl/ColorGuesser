78da6dd7030af1f3ed8ff57e96dcf6b5bb3b1158
Gunay <gunaykaraoglu21@gmail.com>
2023-07-22 13:56:17 +0300

    Split card comp., separate React & CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './Card'
 
 function App() {
 
   return (
-    <>
-      <div className="box"></div>
-      <div className='button-container'>
-        <div className='button'>Button 1</div>
-        <div className='button'>Button 2</div>
-        <div className='button'>Button 3</div>
-        <div className='button'>Button 4</div>
-      </div>
-    </>
+    <Card />
   )
 }
 
---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Card.css"
+
+export default function Card(){
+    return (
+        <div id="container">
+            <div className="box"></div>
+            <input type="text"></input>
+        </div>
+    )
+}
