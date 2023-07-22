0af30b3f61c2182f63c7323b2129276548e2503c
Gunay <gunaykaraoglu21@gmail.com>
2023-07-22 14:21:46 +0300

    Add a  function to fetch JSON data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Card from './components/Card'
 
 function App() {
-
+  const [colorNames, setColorNames] = useState([])
+  async function fetchData() {
+    const res = await fetch("../../css-color-names.json")
+    const data = await res.json()
+    setColorNames(data)
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
+  
   return (
     <Card />
   )
